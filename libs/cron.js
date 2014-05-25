e718698054426936b3016d013de0cb9be088bffc
chenka <teamyakusa@gmail.com>
2014-05-26 02:42:11 +0700

    Load file as sync

--- libs/cron.js
+++ libs/cron.js
@@ -6,31 +6,28 @@ module.exports = Cron = {};
 
 Jobs = []
 
-Cron.loadfile = function(file){
-  fs.readFile(file, 'utf8', function(err, data){
-    if(err){
-      console.error(err);
-    }else{
-      lines = data.split('\n');
-      for (var i = 0; i <= lines.length-1; i++) {
-        line = lines[i];
-        cols = line.split(' ');
+Cron.loadFile = function(file, callback){
+  data = fs.readFileSync(file, 'utf8')
+  lines = data.split('\n');
+  for (var i = 0; i <= lines.length-1; i++) {
+    line = lines[i];
+    cols = line.split(' ');
 
-        schedule = cols.slice(0,5).join(' ');
-        command = cols.slice(5).join(' ');
+    schedule = cols.slice(0,5).join(' ');
+    command = cols.slice(5).join(' ');
 
-        if(command == '') continue;
-        job = {
-          id: makeJobId(line),
-          schedule: schedule,
-          command: command
-        }
-        Jobs.push(job);
-
-        // add to cron
-      }
+    if(command == '') continue;
+    job = {
+      id: makeJobId(line),
+      schedule: schedule,
+      command: command
     }
-  });
+    Jobs.push(job);
+
+  }
+
+  return Jobs
+
 }
 
 makeJobId = function(command){
